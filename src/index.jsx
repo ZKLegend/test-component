cef5d9c4ebbd17f2543acc47ac203e9b24a5dd72
ZKLegend <lekinhanh91@gmail.com>
2023-02-13 17:31:51 +0700

    Edit font style

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById('root')).render(<App/>)
+// import "./fonts/Montserrat/Montserrat-Bold.ttf";
+// import "./fonts/Montserrat/Montserrat-Medium.ttf";
+// import "./fonts/Montserrat/Montserrat-Regular.ttf";
+// import "./fonts/Montserrat/Montserrat-SemiBold.ttf";
+// import "./fonts/TradeGothic/Trade-Gothic-LT-Extended.ttf";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(<App />);
