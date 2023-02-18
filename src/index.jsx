f3cc516dc4af4c133e6e1699127e0e575b3630c1
ZKlegend <lekinhanh91@gmail.com>
2023-02-18 14:53:51 +0700

    Upadte

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 import { ConfigProvider } from "antd";
 import App from "./App";
 
-// import "./fonts/Montserrat/Montserrat-Bold.ttf";
-// import "./fonts/Montserrat/Montserrat-Medium.ttf";
-// import "./fonts/Montserrat/Montserrat-Regular.ttf";
-// import "./fonts/Montserrat/Montserrat-SemiBold.ttf";
-// import "./fonts/TradeGothic/Trade-Gothic-LT-Extended.ttf";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
